e8a3389dbbe4bcf4fa09b9924aa3517e66e12e81
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-07 01:06:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,8 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    erb :new 
+  get '/' do
+    erb :new
   end 
+
+  post '/student' do 
+    @student = Student.new(params[:student])
 
 end
